8d01e02cba60c6e0bac524d677091039e5ccfdbc
amit19-meet <amit19@meet.mit.edu>
2017-07-23 22:17:38 +0300

    Stopping here

---
+++ day1/meetinturtle.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> #Everything that comes after the # is a
+>>> #comment.
+>>> #It is a note to the person reading the code.
+>>> #The computer ignores it.
+>>> #write 
